0c057f31719d62bcde4ff0313786144c3d6db39c
Atychi762 <143887269+Atychi762@users.noreply.github.com>
2023-11-13 18:59:37 +0000

    partially completed post_message.sh

--- post_messages.sh
+++ post_messages.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+# check if the sender id exists
+if [ -d $1 ]; then
+	# check if the receiver id exists
+	if [ -d $2 ]; then
+		# if both ids exist check if they are friends
+		if grep "$1" "$2"/friends.txt 2> /dev/null; then
+			# if they are friends then add the message to the receivers wall file
+			echo ""$1": "$3"" >> "$2"/wall.txt
+			echo 'ok message posted'
+		else 
+			# if they are not friend print the error messgae
+			echo "nok: "$1" is not a friend of "$2" "
+		fi
+	else 
+		# eror sent when receiver id does not exist
+		echo "nok: user "$2" does not exist"
+	fi
+else
+	# error sent when sender id does not exist
+	echo "nok: user "$1" does not exist"
+fi
