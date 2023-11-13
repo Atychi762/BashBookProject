2635dd52de0513bb89e9a66a24fb071b8a60a9c0
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2023-11-13 15:40:39 +0000

    Create.sh is done
    The create.sh is complete. it is working perfectly.

--- create.sh
+++ create.sh
@@ -1,25 +1,15 @@
 #! /bin/bash
 
-if [[$# -eq 0]]; then
+if [ $# -eq 0 ]; then
 	echo 'nok: no idetifier provided'
 
 else
-	for item in { $( ls -d ) }; 
-	do
-		
-		if [ $item = "$1/" ]; then 
-			echo "nok: user already exists"
-			return 1
-		else
-			continue
-		fi
-	done
-	if mkdir $1 > /dev/null; then
-		if [ $? == 0 ]; then 
-			echo 'ok: user created!' 
-		fi
- 		> "$1"/wall.txt
-  		> "$1"/friends.txt
+	if mkdir $1 2>/dev/null; then
+		echo 'ok: user created!' 
+		> "$1"/wall.txt
+		> "$1"/friends.txt
+	else
+		echo "nok: user already exists"
 	fi
-
+	
 fi
