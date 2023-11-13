37cfb2a5900851e6d841b7a314b0915b72dccc04
Atychi762 <143887269+Atychi762@users.noreply.github.com>
2023-11-13 18:41:14 +0000

    removed the check for number of arguements

--- add_friend.sh
+++ add_friend.sh
@@ -1,25 +1,21 @@
 #! /bin/bash
 
-if [ $# -ne 2 ]; then
-	echo 'nok: incorrect number of arguements provided'
 
-else
-	# check if the first id exists
-	if [ -d $1 ]; then
-		# check if the second id exists
-		if [ -d $2 ]; then
-			# if both ids exist check if they are already friends
-			if ! grep "$2" "$1"/friends.txt > /dev/null; then
-				# if they are not friends then add the second user id to the first users friend file
-				echo "$2" >> $1/friends.txt
-				echo 'ok friend added'
-			fi
-		else 
-			# eror sent when user 2 does not exist
-			echo "nok: user "$2" does not exist"
+# check if the first id exists
+if [ -d $1  2> /dev/null ]; then
+	# check if the second id exists
+	if [ -d $2  2> /dev/null ]; then
+		# if both ids exist check if they are already friends
+		if ! grep "$2" "$1"/friends.txt 2> /dev/null; then
+			# if they are not friends then add the second user id to the first users friend file
+			echo "$2" >> $1/friends.txt
+			echo 'ok friend added'
 		fi
-	else
-		# error sent when user 1 does not exist
-		echo "nok: user "$1" does not exist"
+	else 
+		# eror sent when user 2 does not exist
+		echo "nok: user "$2" does not exist"
 	fi
+else
+	# error sent when user 1 does not exist
+	echo "nok: user "$1" does not exist"
 fi
