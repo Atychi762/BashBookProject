b58e4b79df45b9e19f068ecf4551632957e56a6e
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2023-11-13 09:13:13 +0000

    Updating Create.sh to try to fix it
    changing the create file to list the directories, then check if any of them have the same name as the input name. if not, make a new directory for that user.

--- create.sh
+++ create.sh
@@ -3,14 +3,18 @@
 if [[$# -eq 0]]; then
 	echo 'nok: no idetifier provided'
 
-elif [[ls -d | == $1]]; then
-	echo 'nok: user already exists'
-
 else
-	mkdir $1
-	echo 'ok: user created!'
- 	> "$1"/wall.txt
-  	> "$1"/friends.txt
+	for item in ${ls -d}; 
+	do
+		if [item -eq $1]; then 
+			echo "Sorry username already taken. Please pick another username"
+		else
+			mkdir $1
+			echo 'ok: user created!'
+ 			> "$1"/wall.txt
+  			> "$1"/friends.txt
+		fi
+	done
 fi
 
 
