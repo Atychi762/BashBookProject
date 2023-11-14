e923860713a62ed4c4e63cb21e28abc7f4475de4
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2023-11-14 11:08:31 +0000

    added base structure to server script

--- server.sh
+++ server.sh
@@ -1,1 +1,21 @@
 #! /bin/bash
+
+while [0 = 0]
+do 
+    case $1 in
+        create)
+            #do something
+            ;;
+        add)
+            #do something
+            ;;
+        post)
+            #do something
+            ;;
+        display)
+            #do something
+            ;;
+        *)
+            echo "Accepted Commands: {create|add|post|display}"
+
+done
