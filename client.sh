441a92a5572e98713d3a668b6caa78824fd515eb
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2023-11-20 10:01:04 +0000

    finally worked out how to seperate the string into arguments.

--- client.sh
+++ client.sh
@@ -1,6 +1,15 @@
 #! /bin/bash
-mkfifo ""$1"_pipe"
-read inpt > server_pipe
-echo < ""$1"_pipe"
-rm ""$1"_pipe"
+if [ $# -eq 0 ]; then
+    echo "Sorry please enter an id"
+
+else
+    mkfifo ""$1"_pipe"
+    while [ 0 = 0 ]; do
+        IFS=" " read -ra inpt_array
+        for element in "${inpt_array[@]}"; do
+            echo "$element"
+        done
+    done
+    rm ""$1"_pipe"
+fi
 
