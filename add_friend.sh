d914086c4a4438f6f47d470be12dca7109ea1a45
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2023-11-21 14:26:53 +0000

    fixed add friend

--- add_friend.sh
+++ add_friend.sh
@@ -1,29 +1,33 @@
 #! /bin/bash
-
-# adding the arguments to variables for better readability
-userId=$1
-friend=$2
-
-# check if the first id exists as a directory
-if [ -d $id ]; then
-	# check if the second id existss as a directory
-	if [ -d $friend ]; then
-		# if both ids exist check if they are already friends
-		if ! grep "$friend" "$userId"/friends.txt 2> /dev/null; then
-			# if they are not friends then add the second user id to the first users friend file
-			echo "$friend" >> $userId/friends.txt
-			echo 'ok friend added'
-			# Note friendship is not mutual i.e. if user1 friends user2, user1 will not be in user2's friends file
-
-		else
-			# message sent when the users are already friends
-			echo nok: users already friends
-		fi
-	else 
-		# eror sent when user 2 does not exist
-		echo "nok: user "$friend" does not exist"
-	fi
+if [ $# -lt 2 ]; then
+	echo "nok: not enough arguments"
 else
-	# error sent when user 1 does not exist
-	echo "nok: user "$userId" does not exist"
+	# adding the arguments to variables for better readability
+	userId=$1
+	friend=$2
+
+	# check if the first id exists as a directory
+	if [ -d $id ]; then
+		# check if the second id existss as a directory
+		if [ -d $friend ]; then
+			# if both ids exist check if they are already friends
+			if ! grep "$friend" "$userId"/friends.txt 2> /dev/null; then
+				# if they are not friends then add the second user id to the first users friend file
+				echo "$friend" >> $userId/friends.txt
+				echo 'ok friend added'
+				# Note friendship is not mutual i.e. if user1 friends user2, user1 will not be in user2's friends file
+
+			else
+				# message sent when the users are already friends
+				echo nok: users already friends
+			fi
+		else 
+			# eror sent when user 2 does not exist
+			echo "nok: user "$friend" does not exist"
+		fi
+	else
+		# error sent when user 1 does not exist
+		echo "nok: user "$userId" does not exist"
+	fi
 fi
+
