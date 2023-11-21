c5b88f576ad426818f0afbfa8325b5ba7ddd98a6
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2023-11-21 14:20:39 +0000

    adding comments and trap for ctrl+c

--- acquire.sh
+++ acquire.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
-if [ -z "$1" ]; then
-	echo "Usage $0 mutex-name" >&1
-	exit 1
+if [ -z "$1" ]; then #if the first argument is empty
+	echo "Usage: $0 mutex-name" >&1 #print error
+	exit 1 #exit false
 else
-	currentDir=$( pwd )
-	while ! ln "$currentDir$1" "$2"; do
-		echo "fail"
+	currentDir=$( pwd ) #this gets the current working directory and saves it to a var. we did this because we kept running into errors when we tried to acquire the lock 
+						# as it would not be able to find the lock file
+	while ! ln "$currentDir$1" "$2"; do # while the program fails to create a link file sleep 1 second.
 		sleep 1
 	done
-
+	# the while loop will exit once the program succsessfully creates the link file, therefore acquiring the lock.
 	exit 0
 fi
